c9b9f53ab1aa981fdddba03537a829fcf8645a7d
ivanFlor3s <ifloresmanc96@gmail.com>
2023-09-05 20:24:45 -0300

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+const peliculas = [
+    { id: 1, nombre: "El Padrino", año: 1972 },
+    { id: 2, nombre: "Pulp Fiction", año: 1994 },
+    { id: 3, nombre: "El Señor de los Anillos: La Comunidad del Anillo", año: 2001 },
+    { id: 4, nombre: "Titanic", año: 1997 },
+    { id: 5, nombre: "La Lista de Schindler", año: 1993 },
+    { id: 6, nombre: "Star Wars: Episodio IV - Una Nueva Esperanza", año: 1977 },
+    { id: 16, nombre: "Matrix", año: 1999 },
+    { id: 17, nombre: "Forrest Gump", año: 1994 },
+    { id: 18, nombre: "El Rey León", año: 1994 },
+    { id: 19, nombre: "Volver al Futuro", año: 1985 },
+    { id: 20, nombre: "Inception", año: 2010 }
+  ];
+
+</script>
+
+<template>
+  <header>
+    <h1>Lista de peliculas</h1>
+  </header>
+
+  <main>
+    <p>Una de las mejores apps para ordenar una lista de peliculas</p>
+  </main>
+</template>
+
+<style scoped>
+</style>
