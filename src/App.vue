77886a36d1a0f2b9ecc4ebeb5a02f0b6d4acbdf0
ivanFlor3s <ifloresmanc96@gmail.com>
2023-09-05 21:03:39 -0300

    Cards de peliculas, child component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
+import MyPelicula from './components/icons/MyPelicula.vue'
 
 const peliculas = [
-    { id: 1, nombre: "El Padrino", año: 1972 },
-    { id: 2, nombre: "Pulp Fiction", año: 1994 },
-    { id: 3, nombre: "El Señor de los Anillos: La Comunidad del Anillo", año: 2001 },
-    { id: 4, nombre: "Titanic", año: 1997 },
-    { id: 5, nombre: "La Lista de Schindler", año: 1993 },
-    { id: 6, nombre: "Star Wars: Episodio IV - Una Nueva Esperanza", año: 1977 },
-    { id: 16, nombre: "Matrix", año: 1999 },
-    { id: 17, nombre: "Forrest Gump", año: 1994 },
-    { id: 18, nombre: "El Rey León", año: 1994 },
-    { id: 19, nombre: "Volver al Futuro", año: 1985 },
-    { id: 20, nombre: "Inception", año: 2010 }
-  ];
+  { id: 1, nombre: "El Padrino", año: 1972 },
+  { id: 2, nombre: "Pulp Fiction", año: 1994 },
+  { id: 3, nombre: "El Señor de los Anillos: La Comunidad del Anillo", año: 2001 },
+  { id: 4, nombre: "Titanic", año: 1997 },
+  { id: 5, nombre: "La Lista de Schindler", año: 1993 },
+  { id: 6, nombre: "Star Wars: Episodio IV - Una Nueva Esperanza", año: 1977 },
+  { id: 16, nombre: "Matrix", año: 1999 },
+  { id: 17, nombre: "Forrest Gump", año: 1994 },
+  { id: 18, nombre: "El Rey León", año: 1994 },
+  { id: 19, nombre: "Volver al Futuro", año: 1985 },
+  { id: 20, nombre: "Inception", año: 2010 }
+];
 
 </script>
 
@@ -23,8 +24,15 @@ const peliculas = [
 
   <main>
     <p>Una de las mejores apps para ordenar una lista de peliculas</p>
+    <hr>
+
+    <h2>Cards</h2>
+    <div class="d-flex flex-wrap gap-3">
+      <MyPelicula v-for="item in peliculas" :key="item.id" :nombre="item.nombre" :año="item.año" :id="item.id">
+      </MyPelicula>
+    </div>
+
   </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/components/icons/MyPelicula.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+    nombre: string;
+    año: number;
+    id: number;
+}
+    const props = defineProps<Props>()
+</script>
+
+<template>
+    <div class="card p-3">
+        <h3 style="text-wrap: balance"> {{ props.nombre }}</h3>
+        <div class="d-flex justify-content-between gap-3">
+            <span> Año de lanzamiento: {{ props.año }}</span>
+            <span> ID: {{ props.id }}</span>
+        </div>
+    </div>
+</template>
